cfb6a0f7377ec4df70b40c51a346041613972a5b
Julian <julianalvarez199423@gmail.com>
2024-05-17 23:13:24 -0500

    Taller

---
+++ readings/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Book
+
+class bookField (admin.ModelAdmin):
+    list_display=['title','author','genre', 'published_year','isbn','publisher','pages','language','description','cover_image_url']
+
+admin.site.register(Book)
+
+# Register your models here.
