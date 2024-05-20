2839403bdf8377639c18d25074e5f30e797f6842
Julian <julianalvarez199423@gmail.com>
2024-05-20 11:18:46 -0500

    taller1

--- readings/admin.py
+++ readings/admin.py
@@ -4,6 +4,6 @@ from .models import Book
 class bookField (admin.ModelAdmin):
     list_display=['title','author','genre', 'published_year','isbn','publisher','pages','language','description','cover_image_url']
 
-admin.site.register(Book)
+admin.site.register(Book, bookField)
 
 # Register your models here.
--- readings/views.py
+++ readings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Book
+from rest_framework import viewsets
+from .serializador import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 # Create your views here.
